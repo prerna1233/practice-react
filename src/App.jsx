94457b06a93b45fe1f0a542f15b1b630083a5e40
ashwini <ashwiniyadav23@navgurukul.org>
2024-12-19 11:35:48 +0530

    git updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,15 @@ function App() {
     <>
       <h1>Hello India!</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta qui tempora alias quidem at numquam voluptatibus officiis similique totam sit.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error neque commodi blanditiis autem, totam quis, molestiae nostrum odio veritatis illum facilis nisi delectus hic eius quo quae! Laborum, reiciendis cupiditate!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error neque commodi blanditiis autem, totam quis, molestiae nostrum odio veritatis illum facilis nisi delectus hic eius quo quae! Laborum, reiciendis cupiditate!</p>
+
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error neque commodi blanditiis autem, totam quis, molestiae nostrum odio veritatis illum facilis nisi delectus hic eius quo quae! Laborum, reiciendis cupiditate!</p>
+
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error neque commodi blanditiis autem, totam quis, molestiae nostrum odio veritatis illum facilis nisi delectus hic eius quo quae! Laborum, reiciendis cupiditate!</p>
+
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error neque commodi blanditiis autem, totam quis, molestiae nostrum odio veritatis illum facilis nisi delectus hic eius quo quae! Laborum, reiciendis cupiditate!</p>
+
     </>
   )
 }
